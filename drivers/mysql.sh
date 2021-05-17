25ae7079b74c6e34ddae64fd91c70bf37c7f8b97
Sajad Sadrayieh <mss.sadra137812133@gmail.com>
2021-05-17 18:14:37 +0430

    gzip compression

--- drivers/mysql.sh
+++ drivers/mysql.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-FILENAME=$(date +"%Y-%m-%d--%H-%M-%S").bak.sql
+FILENAME=$(date +"%Y-%m-%d--%H-%M-%S").sql.gz
 
 PATH=$(pwd)/backups/$FILENAME
 
 echo "backuping $FILENAME ..."
 
-/usr/bin/mysqldump -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME" -p "$DB_PASSWORD" "$DB_DATABASE" > "$PATH"
+/usr/bin/mysqldump -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME" -p "$DB_PASSWORD" "$DB_DATABASE" | gzip > "$PATH"
 
 echo "backup $FILENAME finished"
