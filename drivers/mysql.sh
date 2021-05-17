16ad036a79fdb8761bdf3354a95a589d5ac33c61
Sajad Sadrayieh <mss.sadra137812133@gmail.com>
2021-05-17 18:05:26 +0430

    Update mysql.sh
    correct the absolute path of mysqldump

--- drivers/mysql.sh
+++ drivers/mysql.sh
@@ -6,6 +6,6 @@ PATH=$(pwd)/backups/$FILENAME
 
 echo "backuping $FILENAME ..."
 
-/bin/mysqldump -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME" -p "$DB_PASSWORD" "$DB_DATABASE" > "$PATH"
+/usr/bin/mysqldump -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME" -p "$DB_PASSWORD" "$DB_DATABASE" > "$PATH"
 
 echo "backup $FILENAME finished"
